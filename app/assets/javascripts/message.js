bcd72c507aeb9acde534f08fc8c76b53c98acf1c
marikotokyo <marikokanefuji@MarikonoMacBook-ea.local>
2018-12-07 21:48:39 +0900

    add_automatic-updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,8 @@
-$(function(){
+$(document).on('turbolinks:load', function() {
   function buildHTML(message){
-      if ( message.image ) {
-        var html =
-         `<div class="message" data-message-id=${message.id}>
+    var imagehtml = (message.image)? `<img class="lower-message__image" src="${message.image}">` : "";
+      var html =
+         `<div class="message" data-message-id = "${message.id}" >
             <div class="upper-message">
               <div class="upper-message__user-name">
                 ${message.user_name}
@@ -19,26 +19,8 @@ $(function(){
             <asset_path src=${message.image} >
           </div>`
         return html;
-      } else {
-        var html =
-         `<div class="message" data-message-id=${message.id}>
-            <div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.date}
-              </div>
-            </div>
-            <div class="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-          </div>`
-        return html;
       };
-    }
+
 $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -62,4 +44,35 @@ $('#new_message').on('submit', function(e){
       });
       return false;
     });
+
+
+  $(function(){
+    var interval = setInterval(function(){
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){
+        var last_message_id = $('.message').last().data('message-id');
+
+        $.ajax({
+          url: location.href,
+          type: 'GET',
+          data: { id: last_message_id },
+          dataType: 'json'
+        })
+
+        .done(function(messages){
+          if (messages.length !== 0){
+            messages.forEach(function(messages){
+              var insertHTML = buildHTML(messages);
+              $('.messages').append(insertHTML);
+            });
+            $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight},"first");
+          }
+        })
+        .fail(function(messages){
+          alert('自動更新に失敗しました');
+        })
+      } else {
+        clearInterval(interval);
+      }
+    } ,5000);
+  });
 });
